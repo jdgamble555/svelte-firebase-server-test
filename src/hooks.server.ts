04fb80fbbdb25dde9392d99976d70d7fb8cddce4
Jonathan Gamble <jdgamble555@gmail.com>
2024-08-27 21:06:00 -0500

    referral policy test

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -2,6 +2,19 @@ import type { Handle } from "@sveltejs/kit";
 
 export const handle: Handle = async ({ event, resolve }) => {
 
-    return resolve(event);
+    const modifiedRequest = new Request(event.request.url, {
+        method: event.request.method,
+        headers: event.request.headers,
+        body: event.request.body,
+        redirect: event.request.redirect,
+        credentials: event.request.credentials,
+        referrer: event.request.referrer
+    });
 
+    const response = await resolve({
+        ...event,
+        request: modifiedRequest,
+    });
+
+    return response;
 };
