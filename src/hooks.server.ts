cfcd48d60218768bcd07726505578ac4392f13c7
Jonathan Gamble <jdgamble555@gmail.com>
2024-08-27 21:02:45 -0500

    test

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -2,20 +2,6 @@ import type { Handle } from "@sveltejs/kit";
 
 export const handle: Handle = async ({ event, resolve }) => {
 
-    const modifiedRequest = new Request(event.request.url, {
-        method: event.request.method,
-        headers: event.request.headers,
-        body: event.request.method !== 'GET' && event.request.method !== 'HEAD' ? await event.request.text() : undefined,
-        redirect: event.request.redirect,
-        credentials: event.request.credentials
-    });
-
-    // Use the modified request in the resolve call
-    const response = await resolve({
-        ...event,
-        request: modifiedRequest,
-    });
-
-    return response;
+    return resolve(event);
 
 };
